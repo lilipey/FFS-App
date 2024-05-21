035da539f7177e8ae5fb79b1ced8f137f55fc46e
lilipey <lisa.peyron@my-digital-school.org>
2024-05-21 12:48:59 +0200

    Adding enctype to update the image

--- resources/views/experiences/edit.blade.php
+++ resources/views/experiences/edit.blade.php
@@ -8,7 +8,7 @@
         <div class="max-w-2xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
-                    <form method="POST" action="{{route('experiences.update',  $experience)}}" id="editexperience" class="space-y-4">
+                    <form method="POST" action="{{route('experiences.update',  $experience)}}" id="editexperience" class="space-y-4" enctype="multipart/form-data">
                         @method('PUT')
                         @csrf
                         <div>
